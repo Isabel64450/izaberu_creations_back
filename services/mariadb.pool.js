62f1d5013a5d9c88ba0c0a68a77ba8c9388b7720
Isabel64450 <isa.dumas64@gmail.com>
2025-08-27 15:00:23 +0200

    mariadb

--- services/mariadb.pool.js
+++ services/mariadb.pool.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise"
 
 const getPool=()=>{
     const pool = mysql.createPool({
-        host:"localhost",
+        host:process.env.MARIA_HOST,
         user: process.env.MARIADB_USER,
         password: process.env.MARIADB_PASSWORD,
         database: process.env.MARIADB_DB,
