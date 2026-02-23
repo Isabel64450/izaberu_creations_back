9985cafe13966c2c7eafb493bfd4f7fb29ecc341
isabel64450 <isa.dumas64@gmail.com>
2026-02-23 09:01:02 +0100

    add brevo config

--- utils.de.auth/sendEmail.js
+++ utils.de.auth/sendEmail.js
@@ -1,25 +1,36 @@
-import nodemailer from 'nodemailer'
+import Brevo from '@brevo/mail';
 import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 
 dotenv.config()
-
-console.log('doutis de send mail '+ process.env.GMAIL_USER)
-console.log(process.env.GMAIL_APP_PASSWORD)
+const client = new Brevo.Client({ apiKey: process.env.BREVO_API_KEY });
+console.log('doutis de send mail '+ process.env.BREVO_USER)
+console.log(process.env.BREVO_API_KEY)
 
 const transporter = nodemailer.createTransport({
-    service:'gmail',
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false, 
     auth:{
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD,
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_API_KEY,
         
     }
 })
-console.log("Email:", process.env.GMAIL_USER);
-        console.log("Mot de passe (4 derniers caractères) :", process.env.GMAIL_APP_PASSWORD?.slice(-4));
+console.log("HOST ACTUEL:", transporter.options.host)
+transporter.verify(function(error, success) {
+   if (error) {
+      console.log(" Connexion SMTP échouée:", error);
+   } else {
+      console.log("Serveur SMTP prêt");
+   }
+});
+console.log("Email:", process.env.BREVO_USER);
+        console.log("Mot de passe (4 derniers caractères) :", process.env.BREVO_API_KEY?.slice(-4));
 export default async function sendEmail({to, subject,html}) {
     try{
         const info= await transporter.sendMail({
-            from: `${process.env.GMAIL_NAME} <${process.env.GMAIL_USER}>`,
+            from: `${process.env.BREVO_NAME} <${process.env.BREVO_USER}>`,
             to,
             subject,
             html,
@@ -35,5 +46,13 @@ export default async function sendEmail({to, subject,html}) {
 export function generateVerificationToken(userId) {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: '1d',
+  });
+}
+
+if (process.argv[1].includes("sendEmail")) {
+  sendEmail({
+    to: process.env.BREVO_USER, 
+    subject: "Test Brevo",
+    html: "<h1>Test réussi </h1>"
   });
 }
